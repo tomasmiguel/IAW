f29008a854bc70b45a3dfed55c7143bbaba4b42b
Germán A. Gómez <germang08@hotmail.com>
2021-02-06 12:44:10 -0300

    Obtener emosiones y sentimientos y pasarlo a vista resultado

---
+++ src/app/index/search/models/song.ts
@@ -0,0 +1,32 @@
+export class Song {
+  public constructor(
+    public artist: {
+      name: string
+    },
+    public track: {
+      name: string,
+      text: string,
+      text_en: string,
+      lang: {
+        code: string,
+        name: string
+      }
+    },
+    public emotion: {
+      sadness: number,
+      joy: number,
+      fear: number,
+      disgust: number,
+      anger: number,
+    },
+    public sentiment: {
+      polarity: number,
+      type: string
+    },
+    public copyright: {
+      notice: string,
+      artist: string,
+      text: string
+    }
+  ){}
+}
--- src/app/index/search/search.component.ts
+++ src/app/index/search/search.component.ts
@@ -1,3 +1,5 @@
+import { Song } from './models/song';
+import { SearchService } from './services/search.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ResultService } from '../results/services/result.service';
@@ -10,31 +12,77 @@ import { ResultService } from '../results/services/result.service';
 export class SearchComponent implements OnInit {
 
   searchForm: FormGroup;
-  songResults: any;
+  song: Song;
   isSearching = false;
 
-  constructor(private resultService: ResultService, private formBuilder: FormBuilder) {
+  constructor(
+    private _search: SearchService,
+    private formBuilder: FormBuilder
+  ) {
     this.searchForm = this.formBuilder.group({
       artist: [{ value: null, disabled: false }, [Validators.required]],
       song: [{ value: null, disabled: false }, [Validators.required]],
     });
-   }
+  }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.search();
+  }
 
-  search(): void {
-    if (this.searchForm.invalid){
-      return;
+  search() {
+    if (!this.searchForm.invalid) {
+      this.getLyrics();
     }
+  }
+
+  async getSentiments() {
+    this._search.getSentiment(this.song.track.text_en).subscribe(
+      ({ result }) => {
+        this.song.sentiment = result;
+        console.log(this.song);
+      }
+    );
+  }
+
+  async getEmotions() {
+    this._search.getEmotions(this.song.track.text_en).subscribe(
+      ({ emotion: { document: { emotion } } }) => {
+        this.song.emotion = emotion;
+        console.log(this.song);
+      }
+    );
+  }
+
+  async translate() {
+    const { translations: [first] } = await this._search.translateText([this.song.track.text], this.song.track.lang.code, 'en').toPromise();
+    this.song.track.text_en = first.translation;
+  }
+
+  async identifyLanguage() {
+    const { languages: [first] } = await this._search.identifyLanguage(this.song.track.text).toPromise();
+    this.song.track.lang.code = first.language;
+  }
+
+  async getLyrics() {
     this.isSearching = true;
     document.getElementById('loading-section')?.scrollIntoView();
 
-    this.resultService.analizeSong().subscribe((response) => {
-      //simulo delay
-      setTimeout(() => {
-        // if llego ok y se sabe el idioma, etc etc..
-        this.songResults = response;
-      }, 3000);
-    });
+    const { artist, song } = this.searchForm.value;
+    this._search.getLyrics(artist, song).subscribe(
+      async ({ result }) => {
+        this.song = result;
+        if (this.song.track.lang.code === 'xx') {
+          await this.identifyLanguage();
+        }
+
+        (this.song.track.lang.code !== 'en')
+          ? await this.translate()
+          : this.song.track.text_en = this.song.track.text;
+
+        this.getEmotions();
+        this.getSentiments();
+      }
+    );
   }
+
 }
