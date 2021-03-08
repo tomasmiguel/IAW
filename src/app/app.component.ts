25d074aeeca8f5393beb841ed88caeab52d208a6
Germán A. Gómez <germang08@hotmail.com>
2021-03-08 17:26:17 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,9 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (!localStorage.getItem('refresh_token') && spotify.redirect_uri !== 'http://localhost:4444/callback') {
+    /* if (!localStorage.getItem('refresh_token') && spotify.redirect_uri !== 'http://localhost:4444/callback') {
       localStorage.setItem('resfresh_token', 'AQBH6kNPMLaUIHjgsT8zEW_KP_dkmP1mRQaCHgTgceRYz_Dsn0KZi1uF_-nEhezYSOhykOVJ_VQpleEzaauVx5bHUKgcG3x1DpsFWLaBBrXHaJl0ay_8Q7Xt77x19GdYE-w');
-    }
+    } */
   }
 
 
